744d7ccc5f804753e26252ec9b67f110de0c06c7
Zeeeeeeju <QUENTIN.LIANG@OOCL.COM>
2020-08-04 17:14:12 +0800

    [Quentin & Eva] feature: init

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from 'react-redux'
+import { createStore } from 'redux';
+import {reducers} from './reducers'
 
+let store = createStore(reducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
