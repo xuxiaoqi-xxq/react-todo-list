81a2c02eaea87463f22fdbf098202a44443f4010
Zeeeeeeju <QUENTIN.LIANG@OOCL.COM>
2020-08-04 17:54:25 +0800

    [Quentin & Eva] feature: add data-id attribute

--- src/components/Todo/index.jsx
+++ src/components/Todo/index.jsx
@@ -7,9 +7,13 @@ class Todo extends React.Component{
         this.props.deleteTodo(this.props.index);
     }
 
+    done = (e)=>{
+        console.log(e.target.getAttribute("data-id"));
+    }
+
     render(){
         return (
-            <div className="todo-border">{this.props.text}<img onClick={this.deleteTodo} src='/delete.png'/></div>
+            <div className="todo-border"><span data-id={this.props.index} onClick={this.done}>{this.props.text}</span><img onClick={this.deleteTodo} src='/delete.png'/></div>
         )
     }
 
