3fd81554c26f46c0137ff673c58876fd30847561
xuxiaoqi-xxq <xxq_work@163.com>
2020-08-04 21:33:47 +0800

    [unfinished][Eva] feature: add router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import './App.css';
-import TodoListContainer from './containers/TodoListContainer'
-import TodoFormContainer from './containers/TodoFormContainer'
+import TodoListContainer from './containers/TodoListContainer';
+import TodoFormContainer from './containers/TodoFormContainer';
+import DoneTodoList from './components/DoneTodoList';
+import { HashRouter, Link, Route, Router, Switch } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <div>
+            <TodoListContainer />
+            <TodoFormContainer />
+        </div>
+    );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <TodoListContainer/>
-        <TodoFormContainer />
+      <HashRouter>
+                <div>
+                    <ul>
+                        <Link to="/done" >Done Todo List</Link>
+                    </ul>
+                    <ul>
+                        <Link to="/" >Add Todo List</Link>
+                    </ul>
+                    <Route path="/done" component={DoneTodoList}></Route>
+                    <Route path="/" component={Home}></Route>
+                </div>
+        </HashRouter>
       </header>
     </div>
   );
---
+++ src/components/DoneTodoList/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class DoneTodoList extends React.Component{
+    render() {
+        return (
+            <div>done todo list</div>
+        );
+    }
+}
+
+export default DoneTodoList;
