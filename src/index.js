e47de41b0fb8f28c741eb5ac501c7e30cd9ae12c
xuxiaoqi-xxq <xxq_work@163.com>
2020-08-05 14:22:51 +0800

    [Eva] feature: add ant design

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux'
 import { createStore } from 'redux';
-import {reducers} from './reducers'
+import {reducers} from './reducers';
+import 'antd/dist/antd.css';
 
 let store = createStore(reducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 ReactDOM.render(
