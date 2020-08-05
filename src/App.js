ad3fe4496ec49dedea7abe8470a41fbef6e12992
xuxiaoqi-xxq <xxq_work@163.com>
2020-08-05 08:31:38 +0800

    [Eva] feature: implement click link will link to different page

--- src/App.js
+++ src/App.js
@@ -3,32 +3,32 @@ import './App.css';
 import TodoListContainer from './containers/TodoListContainer';
 import TodoFormContainer from './containers/TodoFormContainer';
 import DoneTodoList from './components/DoneTodoList';
-import { HashRouter, Link, Route, Router, Switch } from 'react-router-dom';
+import { HashRouter, Link, Route, Switch } from 'react-router-dom';
 
 const Home = () => {
-    return (
-        <div>
-            <TodoListContainer />
-            <TodoFormContainer />
-        </div>
-    );
+  return (
+    <div>
+      <TodoListContainer />
+      <TodoFormContainer />
+    </div>
+  );
 }
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <HashRouter>
-                <div>
-                    <ul>
-                        <Link to="/done" >Done Todo List</Link>
-                    </ul>
-                    <ul>
-                        <Link to="/" >Add Todo List</Link>
-                    </ul>
-                    <Route path="/done" component={DoneTodoList}></Route>
-                    <Route path="/" component={Home}></Route>
-                </div>
+        <HashRouter>
+          <Switch>
+            <div class="nav">
+              <ul>
+                <li><Link to="/done" >Done Todo List</Link></li>
+                <li><Link to="/" >Add Todo List</Link></li>
+              </ul>
+              <Route exact path="/done" component={DoneTodoList}></Route>
+              <Route exact path="/" component={Home}></Route>
+            </div>
+          </Switch>
         </HashRouter>
       </header>
     </div>
